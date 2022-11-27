ce2372e15d2cc307b9b0744c545abf92b97acebd
hisham19119 <89460801+hisham19119@users.noreply.github.com>
2022-11-27 13:39:45 +0200

    mobile-app

---
+++ javascript/mobile.js
@@ -0,0 +1,11 @@
+let img = document.querySelector(".img");
+let container = document.querySelector(".container")
+function phones(x){
+    img.src = x;
+}
+
+function colors (color){
+container.style.background = color;
+}
+
+
